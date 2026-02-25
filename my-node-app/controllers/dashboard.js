ecccb21f19ebbbd5a06d3d5dabf9fccee67633d1
Matthew <smoothdiamond45@gmail.com>
2026-02-25 14:19:32 +0000

    edits for today

--- my-node-app/controllers/dashboard.js
+++ my-node-app/controllers/dashboard.js
@@ -1,27 +1,17 @@
 'use strict';
-const playlist = [
-    {
-        id: 1,
-        title: "Piano Sonata no. 3",
-        artist: "Beethoven"
-    },
-    {
-        id: 2,
-        title: "Piano Sonata No. 7",
-        artist: "Beethoven"
-    },
-      {
-    id: 3,
-    title: "Piano Sonata No. 10",
-    artist: "Beethoven"
-  }
-];
+
+import logger from "../utils/logger.js";
 
 const dashboard = {
   createView(request, response) {
-    response.json(playlist);   
+    logger.info("Dashboard page loading!");
+    
+    const viewData = {
+      title: "Playlist App Dashboard"
+    };
+    
+    response.render('dashboard', viewData);
   },
 };
 
 export default dashboard;
-
--- my-node-app/server.js
+++ my-node-app/server.js
@@ -1,32 +1,17 @@
 'use strict';
 
 import express from 'express';
-import routes from './routes.js'
+import routes from "./routes.js";
+import logger from "./utils/logger.js";
+import { create } from 'express-handlebars';
 
 const app = express();
 const port = 3000;
 
-app.get('/', (request, response) => response.send('This is a test!'));
-app.get("/dashboard", (request, response) => {
-  const playlist = [
-    {
-      id: 1,
-      title: "Piano Sonata No. 3",
-      artist: "Beethoven"
-    },
-    {
-      id: 2,
-      title: "Piano Sonata No. 7",
-      artist: "Beethoven"
-    },
-    {
-      id: 3,
-      title: "Piano Sonata No. 10",
-      artist: "Beethoven"
-    }
-  ];
-  response.json(playlist);
-});
-app.get('/error', (request, response) => response.status(404).end('Page not found.'));
+const handlebars = create({extname: '.hbs'});
+app.engine(".hbs", handlebars.engine);
+app.set("view engine", ".hbs");
 
-app.listen(port, () => console.log(`Express app running on port ${port}!`));
+app.use("/", routes);
+
+app.listen(port, () => logger.info(`Your app is listening on port ${port}`));
